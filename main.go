6229835ca85d7da23fbdc262d5280c8612cee84f
Soren.Liang <soniclr.leung@gmail.com>
2023-09-19 11:17:44 +0800

    crawler init hello

---
+++ main.go
@@ -0,0 +1,18 @@
+// Package crawler
+/**
+ * @Author: Soren.Liang
+ * @Description:
+ * @File: main
+ * @Version: 1.0.0
+ * @Date: 2023/9/19 11:12
+ * @Software : GoLand
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Crawler!")
+}
